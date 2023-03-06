d33f910ffb23bac695b5bc3fd87ef54ebc023f74
Jake Wehder <jw@email.comjake>
2023-03-06 13:33:36 -0600

    commit tree d6cc47035fa9309f38c71fefc650d4b5ed8887ab

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,37 @@
 require_relative './stack'
 
 # your code here
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add(value)
+        @s1.push(value)
+    end
+
+    def remove
+        # find out which stack is empty
+        # add the elements from the stack with elements to the empty stack
+        # pop off the last element
+        until @s1.empty? do
+            @s2.push(@s1.pop)
+        end
+        first = @s2.pop
+        first
+    end
+
+    def peek
+        until @s1.empty?
+            @s2.push(@s1.pop)
+        end
+
+        @s2.peek
+    end
+
+
+
+
+end
